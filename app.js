3f7cb30b463ca152191ced2684e234d79d8004a0
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-23 12:56:44 +0200

    done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // ¿En tu páis de origen es normal dejar propina?
+const button = document.querySelector("button")
+
+button.addEventListener("click", function () {
+
+    let BillAmount = +document.querySelector("#bill-amount").value;
+
+    let Tip = +document.querySelector("#percentage-tip").value;
+
+    let TipAmount = ((BillAmount * Tip) /100)
+    document.querySelector("#tip-amount").value = TipAmount;
+
+    let Total = document.querySelector("#total").value;
+    Total = BillAmount + TipAmount;
+    document.querySelector("#total").value = Total;
+
+});
